243f4f5daa179b70732462b42443c55b136b474d
Adrien-ANTON-LUDWIG <ludwig.adrien0@gmail.com>
2023-04-24 16:10:15 +0000

    Display `polybar` on all monitors

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -6,8 +6,9 @@ polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
-# Launch mybar
-echo "---" | tee -a /tmp/mybar.log
-polybar mybar 2>&1 | tee -a /tmp/mybar.log & disown
-
-echo "Bars launched..."
+# Launch mybar on every screen
+# Source: https://github.com/polybar/polybar/issues/763#issuecomment-392960721
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+	echo $(date "+%Y/%m/%d-%H:%M:%S:%3N") "--- Launching on monitor $m" | tee -a /tmp/mybar.log
+    MONITOR=$m polybar --reload mybar 2>&1 | tee -a /tmp/mybar.log & disown
+done
