936c25caa955cbf3c63e86c8fa0c11f58a98351d
Adrien-ANTON-LUDWIG <adrien.anton-ludwig@epita.fr>
2023-05-05 09:15:42 +0000

    Add You-Should-Use plugin config

---
+++ dot_oh-my-zsh/custom/you-should-use_config.zsh
@@ -0,0 +1,10 @@
+# [You Should Use](https://github.com/MichaelAquilina/zsh-you-should-use) config
+
+# Message position
+export YSU_MESSAGE_POSITION="after"
+
+# Hardcore mode
+export YSU_HARDCORE=1
+
+# Show all aliases
+export YSU_MODE=ALL
