b76f4c220f94030fd820170abbd48192eadc2b37
Adrien-ANTON-LUDWIG <adrien.anton-ludwig@epita.fr>
2023-06-02 23:29:24 +0000

    Add aliases: chezmoi, dragon-drop, exercism, ...

--- dot_oh-my-zsh/custom/aliases.zsh
+++ dot_oh-my-zsh/custom/aliases.zsh
@@ -13,10 +13,16 @@ alias battery='upower -i $(upower -e | rg BAT) | rg --color=never "state|to full
 
 alias v='vim'
 alias b='bat'
+alias t='tree'
+alias f='fd'
+alias c='cd'
+alias m='mkdir'
+alias to='touch'
 
 ## Git
 alias gs='git status -sb'
 alias gadog='git log --all --decorate --oneline --graph'
+alias glr='git pull --rebase'
 
 ## Chezmoi
 alias cz='chezmoi'
@@ -24,10 +30,26 @@ alias czc='chezmoi cd'
 alias czi='chezmoi init'
 alias cza='chezmoi add'
 alias cze='chezmoi edit'
+alias czr='chezmoi remove'
 alias czap='chezmoi apply'
 alias czu='chezmoi update'
 
 ## Ripgrep
 alias -g G='| rg'
 
+## feh
+alias fehf='feh --full-screen'
+
+## Exercism
+alias ex='exercism'
+alias exh='exercism help'
+alias exo='exercism open'
+alias exs='exercism submit'
+alias ext='exercism troubleshoot'
+alias exu='exercism upgrade'
+alias exv='exercism version'
+alias exw='exercism workspace'
+
+## Dragon-drop
+alias dd='dragon-drop --and-exit'
 
